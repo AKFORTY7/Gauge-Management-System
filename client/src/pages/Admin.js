3831f17ca3dd5898de241bdcfc58f0af30625d20
Reynan Kanindot <ekahiyang@gmail.com>
2022-05-15 22:56:03 +1000

    fixed naming and or path errors in Admin.js

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 //import { Link } from 'react-router-dom';
 
-import GaugeForm from '../components/AdminForms/GaugeForm';
-import CategoryForm from '../components/AdminForms/CategoryForm';
-import UserForm from '../components/AdminForms/UserForm';
+import GaugeForm from '../components/AdminForms/gaugeForm';
+import CategoryForm from '../components/AdminForms/categoryForm';
+import UserForm from '../components/AdminForms/userForm';
 
 
 //import adminAuth from '../utils/adminAuth';
