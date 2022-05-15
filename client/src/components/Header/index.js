05041c092462f8fbddaa5919822ed1e7969193ee
Reynan Kanindot <ekahiyang@gmail.com>
2022-05-16 01:12:16 +1000

    modified header to add admin page button for admin login

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -27,6 +27,13 @@ const Header = () => {
               <Link className="btn btn-lg btn-info m-2" to="/me">
                 {Auth.getProfile().data.username}'s profile
               </Link>
+
+              {Auth.isAdmin() ?
+                (<> <Link className="btn btn-lg btn-info m-2" to="/admin">
+                  Admin Page
+                </Link></>) : (<></>)
+              }
+
               <button className="btn btn-lg btn-light m-2" onClick={logout}>
                 Logout
               </button>
